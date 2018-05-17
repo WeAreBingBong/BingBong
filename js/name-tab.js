579375d7c7f0f72f7c1d40b3deecf80c29a59de8
33577 <ukj0806@kaist.ac.kr>
2018-05-17 20:10:40 +0900

    this

--- js/name-tab.js
+++ js/name-tab.js
@@ -34,24 +34,25 @@ function searchtags(name){
 document.getElementById("elements").innerHTML = "";
    dishes.on('value',function(snapshot){
       snapshot.forEach(function(child){
-		
+
          if(name.toLowerCase() == child.key.toLowerCase()) {
-          
-         
+
+
             var flag =  [];
             var toprank = ["","",""];
+            var topranktag = [0,0,0];
 			var everytags = [];
-			
+
             for(var obj in child.val().Hashtags)
             {
-			   
+
                   for(var element in child.val().Hashtags[obj])
                   {
 						//if(element == name){ category = obj; }
 						everytags.push(element);
 						for(var i=0;i<tags.length;i++)
                         {
-                         
+
 
                            if(element.toLowerCase() == tags[i].toLowerCase())
                               {
@@ -62,7 +63,7 @@ document.getElementById("elements").innerHTML = "";
                      if(toprank[0] == "")
                      {
                         toprank[0] = element;
-
+                        topranktag[0] = child.val().Hashtags[obj][element];
                      }
                      else if(child.val().Hashtags[obj][element] > child.val().Hashtags[obj][toprank[0]])
                         {
@@ -70,6 +71,11 @@ document.getElementById("elements").innerHTML = "";
                            toprank[0] = element;
                            toprank[2] = toprank[1];
                            toprank[1] = tmp;
+
+                           var tmptag = topranktag[0];
+                           topranktag[0] = child.val().Hashtags[obj][element];
+                           topranktag[2] = topranktag[1];
+                           topranktag[1] = tmptag;
                         }
                      else
                      {
@@ -77,6 +83,7 @@ document.getElementById("elements").innerHTML = "";
                         {
                            toprank[1] = element;
 
+                           topranktag[1] = child.val().Hashtags[obj][element];
                         }
                         else if(child.val().Hashtags[obj][element] > child.val().Hashtags[obj][toprank[1]])
                         {
@@ -84,6 +91,9 @@ document.getElementById("elements").innerHTML = "";
                            toprank[1] = element;
                            toprank[2] = tmp;
 
+                           var tmptag = topranktag[1];
+                           toprantagk[1] = child.val().Hashtags[obj][element];
+                           topranktag[2] = tmptag;
                         }
                         else
                         {
@@ -91,38 +101,42 @@ document.getElementById("elements").innerHTML = "";
                            {
                               toprank[2] = element;
 
+                              topranktag[2] = child.val().Hashtags[obj][element];
                            }
                            else if(child.val().Hashtags[obj][element] > child.val().Hashtags[obj][toprank[2]])
                            {
 
                               toprank[2] = element;
+
+                              topranktag[2] = child.val().Hashtags[obj][element];
                            }
                         }
                      }
                   }
             }
-			
-		var dbTestRef = database.ref(name+'/Hashtags/Others')
-		
+
+		var dbTestRef = database.ref(name+'/Hashtags/ca')
+
            var buttons2="";
          var name_img = '&nbsp<div class="card"><h2 class="card__title" style="font-family: Quicksand; font-size:21px; font-weight:bold; ">'+child.key+'</h2><div class="card__content"><img src='+child.val().image +' width="180" class = "pull-left">';
- 
+
             var toprankbutton="";
-        
+
             for(var i=0;i<3;i++)
                {
                   if(toprank[i] != "")
                      {
-                        buttons2 += '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;">' + toprank[i]+ '<img src="./img/profile.png" height="16" width="16">'+32+'</button>';
+                        buttons2 += '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;">' + toprank[i]+ '<img src="./img/profile.png" height="16" width="16">'+topranktag[i]+'</button>';
+
                      }
                }
-			var tempb = buttons2;   
+			var tempb = buttons2;
 			var more_button = '<button class = "more" id="morebut"></button>';
             toprankbutton = '<h4 class = "make-margin" style="font-family: Quicksand; font-size:16px;">Top Rank Hashtags </h4><div class = "make-margin">' +buttons2+ " " +more_button+'</div></div>';
-			
+
             document.getElementById("elements").innerHTML=document.getElementById("elements").innerHTML+name_img+toprankbutton;
-			
-			
+
+
 			var tagclick = document.getElementsByClassName("hashb");
 			for (var i = 0; i < tagclick.length; i++) {
 				tagclick[i].addEventListener('click', (function(i) {
@@ -138,10 +152,10 @@ document.getElementById("elements").innerHTML = "";
 					};
 				})(i), false);
 			};
-			
+
 			document.getElementById("morebut").onclick = function() {
 				var addinput = '<input id = "input2" class = "adding" type="text" placeholder="Add Hashtag ex) spicy, salty">';
-				
+
 				for(var i=0;i<everytags.length;i++){
 				//top rank랑 겹치면 안넣어줌
 					if(everytags[i] != toprank[0] && everytags[i] != toprank[1] && everytags[i] != toprank[2]){
@@ -152,21 +166,21 @@ document.getElementById("elements").innerHTML = "";
 				document.getElementById("elements").innerHTML = name_img+ '<h4 class = "make-margin" style="font-family: Quicksand; font-size:16px;">All Hashtags </h4><div class = "make-margin">' +buttons2+ addinput+putButton +more_button+'</div></div>';
 				document.getElementById("morebut").style.height = "0px";
 				document.getElementById("morebut").style.width = "0px";
-				
-		
+
+
 				var put_Button = document.getElementById("put");
 		//console.log(put_Button);
 				put_Button.onclick = function() {
-					addtag(child.key, document.getElementById("input2").value)	
-			
+					addtag(child.key, document.getElementById("input2").value)
+
 				};
 			}
-			
+
          }
-		 
+
          });
       });
-	  
+
 }
 
 
@@ -198,7 +212,7 @@ $(document).ready(function(){
       });
 
 	//if($('div.pannel11').length == 1{
-		
+
 	//}
 	console.log("Updated");
 	//console.log($('div.card__title').length);
@@ -237,14 +251,14 @@ $( "#input1" ).autocomplete({
 
 
 function addtag(child, tag){
-	database.ref(child+'/Hashtags/Others').child(tag).set(1);
+	database.ref(child+'/Hashtags/ca').child(tag).set(1);
 	searchtags(child);
-	
+
 }
 
-	
-	
-	
+
+
+
 var search_button  = document.getElementById("searchbutton");
 search_button.onclick = function(){answerclick()};
 
