f2ed80c08b570e5b37e759c30386a26865695c16
WeAreBingBong <38876059+WeAreBingBong@users.noreply.github.com>
2018-06-06 23:38:47 +0900

    Add files via upload

--- js/name-tab.js
+++ js/name-tab.js
@@ -8,7 +8,7 @@ var dishes;
 var count = 0;
 var tagsauto = [];
 var putButton = '<button class = "foradd" id="put"></button>';
-
+var results = 0;
 /*
 function categoryOfTag(child, tag) {
   dishes.on('value',function(snapshot){
@@ -31,13 +31,14 @@ function categoryOfTag(child, tag) {
 
 //console.log(indexOfTag(Gamjatang, Hot));
 function searchtags(name){
+  results = 0;
 document.getElementById("elements").innerHTML = "";
    dishes.once('value',function(snapshot){
       snapshot.forEach(function(child){
 
          if(name.toLowerCase() == child.key.toLowerCase()) {
 
-
+            results += 1;
             var flag =  [];
             var toprank = ["","",""];
             var topranktag = [0,0,0];
@@ -116,153 +117,52 @@ document.getElementById("elements").innerHTML = "";
             }
 
 
-           var buttons2="";
-         var name_img = '&nbsp<div class="card"><h2 class="card__title" style="font-family: Quicksand; font-size:21px; font-weight:bold; ">'+child.key+'</h2><div class="card__content"><img src='+child.val().image +' width="180" class = "pull-left">';
-
-            var toprankbutton="";
-
-            for(var i=0;i<3;i++)
-               {
-                  if(toprank[i] != "")
-                     {
-                        buttons2 += '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;">' + toprank[i]+ '<img src="./img/profile.png" height="16" width="16">'+topranktag[i]+'</button>';
-
-                     }
-               }
-
-
-			var tempb = buttons2;
-			var more_button = '<button class = "more" id="morebut"></button>';
-            toprankbutton = '<h4 class = "make-margin" style="font-family: Quicksand; font-size:16px;">Top Rank Hashtags </h4><div class = "make-margin">' +buttons2+ " " +more_button+'</div></div>';
-
-            document.getElementById("elements").innerHTML=document.getElementById("elements").innerHTML+name_img+toprankbutton;
-
-
-			var tagclick = document.getElementsByClassName("hashb");
-      //console.log(tagclick[0]);
-
-			for (var i = 0; i < tagclick.length; i++) {
-        tagclick[i].addEventListener('click', (function(i) {
-          //tagclick[i].setAttribute("id", "clickedone");
-          //document.getElementById("clickedone").style.height = "100px";
-					return function() {
-            //console.log(i);
-						//console.log(toprank[i]);
-            var a = toprank[i];
-            var dbTestRef = database.ref(name+'/Hashtags/ca')
-            //console.log(this.style.backgroundColor);
-            //라이크 숫자 업뎃하는 코드
-            var arr = [];
-            dbTestRef.once('value', function(data){
-              //console.log(toprank[i]);
-              var likes = data.child(toprank[i]).val();
-              arr.push(likes);
-              //console.log(arr[0]);
-              //console.log(likes);
-              firebase.database().ref(name+'/Hashtags/ca/'+a).set(likes+1);
-              //console.log(tagclick[i]);
-              //$(this).css('background-color', 'red');
-              //document.getElementById('clickedone').style.backgroundColor="black";
-              //return(data.child(tag).val());
-            })
-            //console.log(arr[0]);
-            this.style.backgroundColor = "yellow";
-            //console.log(tagclick[i].innerHTML.length);
-            //  console.log(tagclick[i].innerHTML[-1]);
-            var str = "";
-            for (var j = 0; j < tagclick[i].innerHTML.length-2; j++){
-              str += tagclick[i].innerHTML[j];
-            }
-            var b = arr[0] + 1;
-            //console.log(str);
-            tagclick[i].innerHTML = str + b;
-
-						//console.log(likes);
-
-            //dbTestRef.update({
-            //  a : likes + 1
-            //})
-					};
-          }) (i), false);
-
-			};
-      //tagclick[i]= '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;">' + toprank[i]+ '<img src="./img/profile.png" height="16" width="16">'+topranktag[i]+'</button>';
-
-			document.getElementById("morebut").onclick = function() {
-				var addinput = '<input id = "input2" class = "adding" type="text" placeholder="Add Hashtag ex) spicy, salty">';
-
-				for(var i=0;i<everytags.length;i++){
-				//top rank랑 겹치면 안넣어줌
-          var dbTestRef = database.ref(name+'/Hashtags/ca')
-          var arr = [];
-          dbTestRef.once('value', function(data){
-              var likes = data.child(everytags[i]).val();
-              arr.push(likes);
-            })
-					if(everytags[i] != toprank[0] && everytags[i] != toprank[1] && everytags[i] != toprank[2]){
-						buttons2 += '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;">' + everytags[i]+ '<img src="./img/profile.png" height="16" width="16">'+arr[0]+'</button>';
-					}
-					else continue;
-				}
-				document.getElementById("elements").innerHTML = name_img+ '<h4 class = "make-margin" style="font-family: Quicksand; font-size:16px;">All Hashtags </h4><div class = "make-margin">' +buttons2+ addinput+putButton +more_button+'</div></div>';
-				document.getElementById("morebut").style.display = 'none';
-
-        var tagclick = document.getElementsByClassName("hashb");
-
-        //위에꺼처럼 노란색 되게
-  			for (var i = 0; i < tagclick.length; i++) {
-          tagclick[i].addEventListener('click', (function(i) {
-  					return function() {
-              var strr = "";
-              for(var k=0; k<tagclick[i].innerHTML.length; k++){
-                if(tagclick[i].innerHTML[k] != "<"){
-                  strr += tagclick[i].innerHTML[k];
-                }
-                else break;
+           var buttons = "";
+        for(var i=0;i<tags.length;i++)
+          {
+            buttons += '<button>' + flag[i]+ '</button>&nbsp';
+          }
+        //console.log(child.val().image);
+        // Adding Hashtag
+        /*var hashtagbutton = '&nbsp<div class = "pannel panel-info" style="margin-top: 2px;margin-bottom: 2px; margin-right: 2px; margin-left: 10px; border:1px solid #bce8f1; border-radius: 5px;" ><div class = "panel-heading"><h4>'+child.key+'</h4></div><div class = "panel-body"><img src='+child.val().image +' width="200" class = "pull-left"><div class = "container" ><h4 class = "make-margin">Searching Hashtags </h4><div class = "make-margin">' +buttons +'</div></div>';*/
+        var hashtagbutton = '&nbsp<div style="margin-top:10px;"class="card"><h2 class="card__title" style="font-family: Quicksand; font-size:21px; font-weight:bold; ">'+child.key+'</h2><div class="card__content"><img src='+child.val().image +' width="180" class = "pull-left">';
+        // Adding Top Rank Hashtag
+        var buttons2=""
+        var toprankbutton="";
+        for(var i=0;i<3;i++)
+          {
+            if(toprank[i] != "")
+              {
+                var tmp = '<button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;" onclick="hashclick(this)">'
+                        + toprank[i]+
+                        ////여기ㅣ기기기건드렸음!!!!!!
+                        '<img src="./img/profile.png" height="16" width="16">'
+                        + topranktag[i]  + //  숫자
+                        '</button>';
+                buttons2 += tmp;
               }
-              // 이게 태그 이름이 됨 이제ㅋㅋ console.log(strr);
-              var dbTestRef = database.ref(name+'/Hashtags/ca')
-              //라이크 숫자 업뎃하는 코드
-              var arr = [];
-              dbTestRef.once('value', function(data){
-                var likes = data.child(strr).val();
-                arr.push(likes);
-                firebase.database().ref(name+'/Hashtags/ca/'+strr).set(likes+1);
-              })
-              this.style.backgroundColor = "yellow";
-              var str = "";
-              var iter = 0;
-              if (i<3){
-                iter = tagclick[i].innerHTML.length-2;
-                var b = arr[0] +1;
-              }
-              else{
-                iter = tagclick[i].innerHTML.length-1;
-                var b = arr[0]+1;
-              }
-              for (var j = 0; j < iter; j++){
-                str += tagclick[i].innerHTML[j];
-              }
-              tagclick[i].innerHTML = str + b;
-  					};
-            }) (i), false);
+          }
 
-  			};
+        
 
+      
+        var name_img = '&nbsp<div class="card"><h2 class="card__title" style="font-family: Quicksand; font-size:21px; font-weight:bold; ">'+child.key+'</h2><div class="card__content"><img src='+child.val().image +' width="180" class = "pull-left">';
+        
+        var tempb = buttons2;
+        var more_button = '<br><i class="fa fa-caret-square-down" id="morebut" onclick="showmore(this.parentNode.parentNode.parentNode)" style = "color:gray;font-size:30px;"></i>';
+        
+              toprankbutton = '<h4 class = "make-margin" style="font-family: Quicksand; font-size:16px;">Top Rank Hashtags </h4><div class = "make-margin">' +buttons2+more_button+'</div></div>';
 
+              document.getElementById("elements").innerHTML=document.getElementById("elements").innerHTML+name_img+toprankbutton;
+      }
 
-				var put_Button = document.getElementById("put");
-		//console.log(put_Button);
-				put_Button.onclick = function() {
-					addtag(child.key, document.getElementById("input2").value)
-
-				};
-			}
-
-         }
-
-         });
       });
+  if(results == 0 && tags.length !=0)
+  {
+    document.getElementById("elements").innerHTML = '<div style="color:gray; margin-top:10px;">&nbsp&nbspNo Results</div>';
+  }
+  });
+  
 
 }
 
@@ -324,13 +224,111 @@ $( "#input1" ).autocomplete({
     });
 
 
+function showmore(node1)
+{
+  //버튼을 누른 음식 이름
+  var toprank = [];
+  //console.log(node1.childNodes[1].childNodes[2].childNodes[0]);
+  toprank.push(node1.childNodes[1].childNodes[2].childNodes[0].innerHTML.split('<img')[0]);
+  toprank.push(node1.childNodes[1].childNodes[2].childNodes[1].innerHTML.split('<img')[0]);
+  toprank.push(node1.childNodes[1].childNodes[2].childNodes[2].innerHTML.split('<img')[0]);
+  
+  node1.childNodes[1].childNodes[1].innerHTML = "All Hashtags";
 
-function addtag(child, tag){
-	database.ref(child+'/Hashtags/ca').child(tag).set(1);
-	searchtags(child);
+  //console.log(node1.childNodes[1].childNodes[2].childNodes[3]);
+  node1.childNodes[1].childNodes[2].removeChild(node1.childNodes[1].childNodes[2].childNodes[3]);
+  node1.childNodes[1].childNodes[2].removeChild(node1.childNodes[1].childNodes[2].childNodes[3]);
+  var name = node1.childNodes[0].innerHTML;
+  var nameRef = database.ref(name+'/Hashtags/ca')
+
+  nameRef.once('value',function(snapshot){
+    snapshot.forEach(function(child){
+      if(child.key != toprank[0] && child.key != toprank[1] && child.key != toprank[2])
+      { 
+        node1.childNodes[1].childNodes[2].innerHTML = node1.childNodes[1].childNodes[2].innerHTML + ' <button class="hashb" style="margin-bottom : 5px; font-family: Quicksand;" onclick="hashclick(this)">'+child.key+'<img src="./img/profile.png" height="16" width="16">'+child.val()+'</button>';
+      }
+    });
+  });
+
+  var addinput = '<input id = "input2" style = "width:270px; height:40px;"class = "adding" type="text" placeholder="Add Hashtag ex) spicy, salty"></input><i style = "font-size: 30px; color:gray ;margin-left:10px; margin-top:5px;"class = "fa fa-plus-square" id="put" onclick="addnewhash(this.parentNode.parentNode)"></i><i style = "font-size: 30px; color:gray ;margin-left:10px; margin-top:5px;" class = "make-margin fa fa-caret-square-up" onclick = "fold(this.parentNode.parentNode)"></i>';
+  
+
+  node1.childNodes[1].innerHTML = node1.childNodes[1].innerHTML+addinput;
+}
+
+function addnewhash(node)
+{
+  var name = node.childNodes[0].innerHTML;
+  var tag = node.childNodes[1].childNodes[3].value;
+  node.childNodes[1].childNodes[3].value="";
+  if(tag != "")
+  {
+    var flag = 0;
+    var nameRef = database.ref(name+'/Hashtags/ca');
+    /*nameRef.once('value',function(snapshot){
+    snapshot.forEach(function(child){
+
+      if(child.key == tag)
+      { 
+        console.log(child.val())
+        nameRef.child(tag).set(child.val()+1)
+        flag = 1;
+      }
+    });
+    if(flag == 0)
+    {*/
+      nameRef.child(tag).set(1);
+      node.childNodes[1].childNodes[2].innerHTML = node.childNodes[1].childNodes[2].innerHTML + ' <button class="hashb" style="margin-bottom : 5px; font-family: Quicksand; background-color:lightgray;" onclick="hashclick(this)">'+tag+'<img src="./img/profile.png" height="16" width="16">'+"1"+'</button>';
+    //}
+
+  }
+  
+  //ode1.childNodes[1].childNodes[2].innerHTML = node1.childNodes[1].childNodes[2].innerHTML + ' <button class="hashb yellow" style="margin-bottom : 5px; font-family: Quicksand;" onclick="hashclick(this)">'+child.key+'<img src="./img/profile.png" height="16" width="16">'+"1"+'</button>';
+  //console.log(tag);
+}
+
+function hashclick(node)
+{ 
+  //console.log(node);
+  if(node.style.backgroundColor!="lightgray")
+  {
+    node.style.backgroundColor="lightgray";
+    //console.log(node.innerHTML.split('>')[1])
+    var added = parseInt(node.innerHTML.split('>')[1])+1;
+    node.innerHTML=node.innerHTML.split('>')[0]+'>'+added;
+    //console.log(node.parentNode.parentNode.parentNode);
+    var node1 = node.parentNode.parentNode.parentNode;
+    var name = node1.childNodes[0].innerHTML;
+    var nameRef = database.ref(name+'/Hashtags/ca');
+
+    var tagname = node.innerHTML.split('<img')[0];
+
+    nameRef.child(tagname).set(added);
+  }
+  
 
 }
 
+function fold(node)
+{
+  var more_button = '<br><i class="fa fa-caret-square-down" id="morebut" onclick="showmore(this.parentNode.parentNode.parentNode)" style = "color:gray;font-size:30px;"></i>';
+  //console.log(node.childNodes[1].childNodes[2]);
+  var name = node.childNodes[0].innerHTML;
+  node.childNodes[1].childNodes[1].innerHTML = "Top Rank Hashtags";
+  var toprank = [];
+  toprank[0] = node.childNodes[1].childNodes[2].childNodes[0];
+  toprank[1] = node.childNodes[1].childNodes[2].childNodes[1];
+  toprank[2] = node.childNodes[1].childNodes[2].childNodes[2];
+  node.childNodes[1].childNodes[2].innerHTML="";
+  node.childNodes[1].childNodes[2].appendChild(toprank[0]);
+  node.childNodes[1].childNodes[2].appendChild(toprank[1]);
+  node.childNodes[1].childNodes[2].appendChild(toprank[2]);
+  node.childNodes[1].childNodes[2].innerHTML = node.childNodes[1].childNodes[2].innerHTML + more_button;
+  console.log(node.childNodes[1].childNodes[3]);
+  node.childNodes[1].removeChild(node.childNodes[1].childNodes[3]);
+  node.childNodes[1].removeChild(node.childNodes[1].childNodes[3]);
+  node.childNodes[1].removeChild(node.childNodes[1].childNodes[3]);
+}
 
 
 
